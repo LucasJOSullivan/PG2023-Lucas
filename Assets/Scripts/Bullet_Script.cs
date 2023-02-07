e1ff8ebd5ddcf5e5a7cf2aeb9bc53490306c428d
STUDENT Lucas J. O Sullivan <Lucas.J.OSullivan@students.ittralee.ie>
2023-02-07 16:01:21 +0000

    Signed-off-by: STUDENT Lucas J. O Sullivan <Lucas.J.OSullivan@students.ittralee.ie>

--- Assets/Scripts/Bullet_Script.cs
+++ Assets/Scripts/Bullet_Script.cs
@@ -4,35 +4,54 @@ using UnityEngine;
 
 public class Bullet_Script : MonoBehaviour
 {
+    Rigidbody rb;
     bool live;
-    float drag, gravity, velocity;
+    float dragsum, drag, speed;
+    Vector3 velocity, gravity;
+    //public ConstantForce gravity;
+
     // Start is called before the first frame update
     void Start()
     {
-        live = true;
-        drag = 1.00f;
-        gravity = 9.81f;
-        velocity = 250.00f;  
+        rb = GetComponentInChildren<Rigidbody>();
+        speed = 5.00f;
+        rb.velocity  =  transform.forward * speed;
+     
+     
+        //drag = 1.00f;
+        //dragsum  = (speed -= drag) * Time.deltaTime;
+
+        //gravity = gameObject.AddComponent<ConstantForce>();
+        //gravity.force = new Vector3(0.0f, -9.81f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
-    {
 
+    {
+        rb.AddForce(new Vector3(0.00f, -9.81f, 0.00f));
+       // gravity = new Vector3(0.00f, -9.81f, 0.00f);
+      //  velocity += gravity * Time.deltaTime;
+     //   transform.position += velocity * Time.deltaTime;
     }
 
+    /*
     void FixedUpdate()
     {
+
         if (live = false)
         {            
             Destroy(this);
         }
     }
+    */
 
+    
     void OnCollisionEnter(Collision collision)
     {
-
-        live = false;
+        speed = 0.00f;
+    //    live = false;
     }
+    
 
 }
