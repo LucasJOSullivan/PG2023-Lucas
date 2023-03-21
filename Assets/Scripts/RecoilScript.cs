73213b05419ec99a5ec85c439ad56dbb7ba05d23
STUDENT Lucas J. O Sullivan <Lucas.J.OSullivan@students.ittralee.ie>
2023-03-21 16:08:50 +0000

    Signed-off-by: STUDENT Lucas J. O Sullivan <Lucas.J.OSullivan@students.ittralee.ie>

--- Assets/Scripts/RecoilScript.cs
+++ Assets/Scripts/RecoilScript.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RecoilScript : MonoBehaviour
 {
+    enum recoilState { None, Recoiling, Returning}
+
+    Vector3 defaultPosition, startingPosition;
+    Quaternion defaultOrientation;
+    recoilState currently = recoilState.None;
+    Vector3 destination;
+    Quaternion destinationOrientation, startingOrientation;
+
+    float timer;
     public bool allowAutoRotationRecovery, allowAutoMovementRecovery;
-    public float cooldownTime,
+    public float cooldownTime, rotationClamp, movementClamp,
+
         upwardRotation, rightwardRotation, downwardRotation, leftwardRotation,
         upwardRotationLimit, rightwardRotationLimit, downwardRotationLimit, leftwardRotationLimit,
         upwardRotationChance, rightwardRotationChance, downwardRotationChance, leftwardRotationChance,
@@ -14,16 +25,90 @@ public class RecoilScript : MonoBehaviour
         forwardMovementLimit, rearwardMovementLimit, upwardMovementLimit, rightwardMovementLimit, downwardMovementLimit, leftwardMovementLimit,
         forwardMovementChance, rearwardMovementChance, upwardMovementChance, rightwardMovementChance, downwardMovementChance, leftwardMovementChance;
 
-    float cooldown_timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        cooldown_timer = 0.0f;
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        switch(currently)
+        {
+            case recoilState.Recoiling:
+
+                transform.localPosition = Vector3.Lerp(startingPosition, destination, timer);
+                transform.localRotation = Quaternion.Slerp(startingOrientation, destinationOrientation, timer);
+
+                timer += Time.deltaTime;
+
+                if (timer >1 )
+                {
+                    currently = recoilState.Returning;
+
+                    startingPosition = destination;
+                    destination = defaultPosition;
+                    startingOrientation = destinationOrientation;
+                    destinationOrientation = defaultOrientation;
+                    timer = 0;
+                }
+                break;
+
+
+
+            case recoilState.Returning:
+
+
+                transform.localPosition = Vector3.Lerp(startingPosition, destination, timer);
+                transform.localRotation = Quaternion.Slerp(startingOrientation, destinationOrientation, timer);
+
+                timer += Time.deltaTime;
+
+                if (timer > 1)
+                {
+                    transform.position = defaultPosition;
+                    transform.rotation = defaultOrientation;
+                    currently = recoilState.None;
+                }
+
+
+                break;
+
+        }
+
+
+
+    }
+
+    internal void startRecoil()
+    {
+        currently = recoilState.Recoiling;
+        timer = 0;
+        destination = getRandomRecoilPosition();
+        startingPosition = transform.localPosition;
+        startingOrientation = transform.localRotation;
+
+        destinationOrientation = getMaximumrecoilOrientation();
+
+
+    }
+
+    internal void setDefaults(Vector3 defaultLocalPosition,Quaternion defaultLocalOrientation)
+    {
+        defaultPosition = defaultLocalPosition;
+        defaultOrientation = defaultLocalOrientation;
+    }
+
+    private Quaternion getMaximumrecoilOrientation()
+    {
+        return Quaternion.AngleAxis(45, transform.right);
+    }
+
+    private Vector3 getRandomRecoilPosition()
+    {
+        return new Vector3(0, 1, -1);
     }
 }
